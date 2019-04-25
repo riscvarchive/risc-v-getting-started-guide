36d12d2d62c20f981151fc12eb968cc2950efe02
Michael Gielda <mgielda@antmicro.com>
2019-04-25 13:42:41 +0200

    Update copyright

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 project = u'RISC-V - Getting Started Guide'
 basic_filename = u'risc-v--getting-started-guide'
 authors = u'RISC-V Foundation'
-copyright = u'Antmicro, 2018; RISC-V Foundation, 2018-2019'
+copyright = u'Antmicro, 2018-2019; RISC-V Foundation, 2019'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
