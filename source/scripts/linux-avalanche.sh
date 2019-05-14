1e112801a324d6b48daeccf668bf3113c5af5cb7
Michael Gielda <mgielda@antmicro.com>
2019-05-14 04:36:47 +0200

    litex/vexriscv + linux: standardise script

--- source/scripts/linux-avalanche.sh
+++ source/scripts/linux-avalanche.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# get prerequisites
+sudo apt install autoconf automake autotools-dev bc bison build-essential curl flex \
+                 gawk gdisk git gperf libgmp-dev libmpc-dev libmpfr-dev libncurses-dev \
+                 libssl-dev libtool patchutils python screen texinfo unzip zlib1g-dev
+# /get prerequisites
+
 # clone
 git clone https://github.com/buildroot/buildroot
 cd buildroot
@@ -17,18 +23,22 @@ make -j`nproc`
 cp output/images/Image output/images/rootfs.cpio ~/vexriscv-bins
 # /build
 
-# prebuilt_binaries
+# get bitstream
+wget https://github.com/riscv/risc-v-getting-started-guide/releases/download/tip/bitstream-litex-vexriscv-avalanche-linux.job
+# /get bitstream
+
+# get prebuilt binaries
 cd ~/vexriscv-bins
 wget https://github.com/riscv/risc-v-getting-started-guide/releases/download/tip/devicetree-litex-vexriscv-avalanche-linux.dtb
 wget https://github.com/riscv/risc-v-getting-started-guide/releases/download/tip/emulator-litex-vexriscv-avalanche-linux.bin
-# /prebuilt_binaries
+# /get prebuiltbinaries
 
-# flashing
+# flash
 # get the litex_term tool
 wget https://raw.githubusercontent.com/enjoy-digital/litex/190ff89aaa120cc983ccaeb1077ba1d23f00e37c/litex/tools/litex_term.py
 wget https://raw.githubusercontent.com/antmicro/risc-v-getting-started-guide/master/source/files/images-litex-vexriscv-avalanche-linux.json
 chmod +x litex_term.py
 # assuming the board serial has been registered as ttyUSB0
 ./litex_term.py --images images-litex.json /dev/ttyUSB0
-# /flashing
+# /flash
 
